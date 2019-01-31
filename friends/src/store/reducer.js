a4548c3da8eab7dc7a4c8aa27d51fc8a005a230f
WebMats <lorlando127@gmail.com>
2019-01-31 14:15:09 -0500

    connected redux store

---
+++ friends/src/store/reducer.js
@@ -0,0 +1,16 @@
+
+
+
+
+const initialState = {
+    friends: []
+}
+
+export default (state=initialState, action) => {
+    switch(action.type) {
+
+
+        default:
+            return state;
+    }
+}
